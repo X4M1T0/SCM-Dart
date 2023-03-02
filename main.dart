30db5e49cd6740ac8378cd3978383725ca2d7783
X4M1T0 <gabrielsantossousa05@hotmail.com>
2023-03-02 00:20:56 -0300

    version 1.2

--- main.dart
+++ main.dart
@@ -7,25 +7,22 @@ void main() {
   print('Seja bem vindo ao nosso sistem de cálculo de notas!');
 
   print('Deseja quais tipos de cálculo de média?');
-  print('1 - Média Simples');
-  print('2 - Calculo de notas');
+  print('1 - Cálculo de Média');
+  print('2 - Cálculo de notas');
   print('3 - MODA');
   print('4 - MEDIANA');
-
+  print('');
   print('*** Selecione os itens de acordo com os seus respectivos números ***');
 
   String? estilo = stdin.readLineSync();
 
-  if(estilo == "bimestre"){
-    calculoBimestral();
-  } else if (estilo == "trimestre"){
-    calculoTrimestral();
-  }
-
+  // SELECIONANDO: MÉDIA SIMPLES
   if(estilo == "1"){
+    
     calculoMedia();
   }
-  
+
+  // SELECIONANDO: CALCULO DE NOTAS
 }
 
 
@@ -33,17 +30,26 @@ void main() {
       CALCULO MÉDIA SIMPLES
 */
 
-void calculoMedia(){
-  int x = 0;
+void calculoMedia(String itens){
+  int x = 1;
+  int somaValue = 0;
   print('Quantos valores deverão ser calculados?');
 
   final String? quest = stdin.readLineSync();
 
   final int? val = int.tryParse(quest!);
 
-  for(x = 0; x < val!; x++){
-    print('${x+1}º valor?');
-    final String? value + "${x+1}"
+  for(x = 1; x <= val!; x++){
+    print('$xº valor?');
+    final String? value = stdin.readLineSync();
+    final int? valueInt = int.tryParse(value!);
+    somaValue = valueInt! + (somaValue);
+    //print('val= $val ; x= $x ; somaValue= $somaValue');
+
+    if(x == val){
+      final completeValue = somaValue / x;
+      print('Seu valor de média é: $completeValue');
+    }
   }
 }
 
