efa6e85f5526d57ea8d5169259a10b17a2a9558c
X4M1T0 <gabrielsantossousa05@hotmail.com>
2023-03-06 18:55:06 -0300

    Version 1.1

--- main.dart
+++ main.dart
@@ -4,12 +4,17 @@ import 'dart:io';
 
 
 void main() {
+
+
   print('Seja bem vindo ao nosso sistem de cálculo de notas!');
+  print('');
+  print('*** Digite "help" para obter ajuda ***');
+  print('');
 
   print('Deseja quais tipos de cálculo de média?');
   print('1 - Cálculo de Média');
-  print('2 - Cálculo de notas');
-  print('3 - MODA');
+  print('2 - Média Empresárial');
+  print('3 - Notas Escolares');
   print('4 - MEDIANA');
   print('');
   print('*** Selecione os itens de acordo com os seus respectivos números ***');
@@ -18,8 +23,20 @@ void main() {
 
   // SELECIONANDO: MÉDIA SIMPLES
   if(estilo == "1"){
-    
     calculoMedia();
+  } else if (estilo == "3"){
+    print('Deseja calcular em que modelo?');
+    print('1 - Bimestral');
+    print('2 - Trimestral');
+    print('3 - Anual');
+    print('');
+    print('*** Selecione os itens de acordo com os seus respectivos números ***');
+  } else if(estilo == "help"){
+    help();  
+  } else {
+    print('Erro: Escolha uma opção!!');
+    print('*** Digite "help" para obter ajuda ***');
+    main();
   }
 
   // SELECIONANDO: CALCULO DE NOTAS
@@ -30,7 +47,7 @@ void main() {
       CALCULO MÉDIA SIMPLES
 */
 
-void calculoMedia(String itens){
+void calculoMedia(){
   int x = 1;
   int somaValue = 0;
   print('Quantos valores deverão ser calculados?');
@@ -54,26 +71,52 @@ void calculoMedia(String itens){
 }
 
 
-/*
-      CALCULO BIMESTRAL
-*/
-
-void calculoBimestral(){
-  int x = 0;
-
-  print('Quantas matérias foram realizadas no ultimo bimestre?');
-  var i = stdin.readLineSync();
-  /*for(int m = 0; m < i; m++){
-    print('Qual a nota da ${x+1} matéria?');
-    final nota = stdin.readLineSync();
-  }*/
-}
 
 
-/*
-      CALCULO TRIMESTRAL
-*/
+    /*
+          CALCULO BIMESTRAL
+    */
 
-void calculoTrimestral(){
+    void calculoBimestral(){
+      int x = 0;
 
+      print('Quantas matérias foram realizadas no ultimo bimestre?');
+      var i = stdin.readLineSync();
+      /*for(int m = 0; m < i; m++){
+        print('Qual a nota da ${x+1} matéria?');
+        final nota = stdin.readLineSync();
+      }*/
+    }
+
+
+    /*
+          CALCULO TRIMESTRAL
+    */
+
+    void calculoTrimestral(){
+
+    }
+
+
+void help(){
+  print('Preisando de uma ajuda? Seguem a baixo os comandos do SCM!');
+  print('');
+
+  // Comandos Básicos
+  print('Comandos Básicos:');
+  print('');
+  print('Help - Abre a central de comandos');
+  print('Return - Retorna ao passo anterior');
+  print('Reload - Reinicia o programa');
+  print('');
+
+  //Explicando cálculos
+  print('Cálculos:');
+  print('');
+
+    //Cálculo de média
+    print('Calculo de média - Calculo de média baseadas nos valores implementados pelo usuário:');
+    print('1 - Escolha a quantidade de valores inseridos;');
+    print('2 - Adicione os valores;');
+    print('3 - Receba o valor do cálculo instataneamente.');
 }
