010f44925114fb919b085752f2a18896d907c6a3
X4M1T0 <gabrielsantossousa05@hotmail.com>
2023-03-09 23:31:32 -0300

    version 1.2

--- Dart/main.dart
+++ Dart/main.dart
@@ -1,29 +1,40 @@
 
 // BIBLIOTECAS
+import 'dart:ffi';
 import 'dart:io';
+import 'dart:mirrors';
 
 
 // VARIÁVEIS
 int etapa = 0;
-String? condition = "";
-String? estilo = "";
+
+String? condition;
+
+String? estilo;
+
 bool fim = false;
 
+String modeloEscolar = 'Deseja calcular em que modelo? \n'
+      '1 - Bimestral \n'
+      '2 - Trimestral \n'
+      '3 - Anual \n'
+      '4 - Taxa de presença';
 
 void main() {
-  clear();
+  commandLine("clear");
 
   print('Seja bem vindo ao nosso sistem de cálculo de notas! \n \n'
   '*** Digite "help" para obter ajuda *** \n\n'
   'Deseja quais tipos de cálculo de média? \n'
   '1 - Cálculo de Média \n'
   '2 - Média Empresárial \n'
-  '3 - Notas Escolares \n'
+  '3 - Escolar \n'
   '4 - MEDIANA \n \n'
   '*** Selecione os itens de acordo com os seus respectivos números ***');
 
   condition = stdin.readLineSync();
   etapa = 0;
+  fim = false;
   analiseDeEtapa(condition, etapa, fim);
   
 
@@ -36,7 +47,8 @@ void main() {
 */
 
 void calculoMedia(){
-  clear();
+  commandLine("clear");
+
   int x = 1;
   int somaValue = 0;
   print('Quantos valores deverão ser calculados?');
@@ -45,14 +57,13 @@ void calculoMedia(){
 
   final int? val = int.tryParse(quest!);
   if(val! < 2){
-    print('**** A QUANTIDADE DE VALORES DEVE SER MAIOR QUE "2" ****');
+    print('\n **** A QUANTIDADE DE VALORES DEVE SER MAIOR QUE "2" **** \n');
     calculoMedia();
   }
 
   for(x = 1; x <= val; x++){
     print('$xº valor?');
-    final String? value = stdin.readLineSync();
-    final int? valueInt = int.tryParse(value!);
+    int? valueInt = int.tryParse(stdin.readLineSync()!);
     somaValue = valueInt! + (somaValue);
     //print('val= $val ; x= $x ; somaValue= $somaValue');
 
@@ -60,7 +71,7 @@ void calculoMedia(){
       final completeValue = somaValue / x;
       print('Seu valor de média é: $completeValue');
 
-      fim == true;
+      fim = true;
       etapa = 10;
       analiseDeEtapa(condition!, etapa, fim);
     }
@@ -74,40 +85,161 @@ void calculoMedia(){
       CALCULO BIMESTRAL
 */
 
-void calculoBimestral(){
-  clear();
-  int x = 0;
+void calculoNotaEscolar(modelo){
+  commandLine("clear");
 
-  print('Quantas matérias foram realizadas no ultimo bimestre?');
-  var i = stdin.readLineSync();
-  /*for(int m = 0; m < i; m++){
-    print('Qual a nota da ${x+1} matéria?');
-    final nota = stdin.readLineSync();
-  }*/
+
+  int x;
+  int valSoma = 0;
+
+  if(modelo == "1"){
+    print('Quantas matérias foram realizadas no ultimo bimestre?');
+    int? i = int.tryParse(stdin.readLineSync()!);
+
+    if(i! < 2){
+      print('\n **** A QUANTIDADE DE VALORES DEVE SER MAIOR QUE "2" **** \n');
+      calculoNotaEscolar(modelo);
+    }
+
+    for(x = 0; x < i; x++){
+      print('Qual a nota da ${x+1} matéria?');
+      int? valNota = int.tryParse(stdin.readLineSync()!);
+      valSoma = valNota! + (valSoma);
+    }
+
+  } else if (modelo == "2"){
+
+  }
+  else if (modelo == "3"){
+
+  } else if (modelo == "4"){
+    // Variable Area
+    String mode = "";
+
+    print('Qual a taxa minima de presença em sua escola/faculdade? \n\n'
+    '** ESCREVA SOMENTE COM NÚMEROS ** \n');
+    double valTax = double.parse(stdin.readLineSync()!);
+
+    if (valTax > 0){
+      valTax = valTax / (100);
+    }
+
+    commandLine("clear");
+    print(valTax);
+    print('Você estuda por qual método? \n' 
+    '1- bimestre \n' 
+    '2- trimestre');
+    var modeloMensal = stdin.readLineSync();
+
+    commandLine("clear");
+    if(modeloMensal == "1"){
+      mode = 'bimestre';
+    } else if(modeloMensal == "2"){
+      mode = 'trimestre';
+    }
+
+    print('Quantas aulas você tem por dia?');
+    int? aulas = int.tryParse(stdin.readLineSync()!);
+
+    commandLine("clear");
+    print('Qual sua quantidade de dias com falta no ultimo $mode');
+    double faltas = double.parse(stdin.readLineSync()!);
+
+    int aulasMes = aulas! * (22);
+
+    if(modeloMensal == "1"){
+      print(valTax);
+      aulasMes = aulasMes * (2);
+      double valueTax1 = aulasMes * (valTax);
+
+      faltas = aulasMes - ((faltas) / (100));
+      print('Você teria que ir $valTax dias');
+
+      double qtdFaltas = faltas * (aulas);
+
+    } else if (modeloMensal == "2"){
+      aulasMes = aulasMes * (3);
+    }
+
+  }
+
 }
 
-
-/*
-      CALCULO TRIMESTRAL
-*/
-
-void calculoTrimestral(){
-  clear();
-}
 
 
 /*
       CALCULO MÉDIA EMPRESARIAL
 */
 void mediaEmpresarial(){
-  clear();
+  commandLine("clear");
 }
 
 
 
+void analiseDeEtapa(condition, etapa, fim){
+  // print('entrou em analise de etapa com $condition');
+
+  if(condition == "help"){
+    help();
+  }
+
+  if (etapa == 0){
+    if(condition == "1"){
+      calculoMedia();
+    } else if (condition == "3"){
+      commandLine("clear");
+
+      print(modeloEscolar); 
+      String? modelo = stdin.readLineSync();
+      calculoNotaEscolar(modelo!);
+    } else {
+      commandLine("clear");
+      
+      print('Erro: Escolha uma opção!! \n'
+      '*** Digite "help" para obter ajuda ***');
+      main();
+    }
+  }
+
+  if(fim == true && etapa >= 10){
+    print ('Para onde deseja ir? \n\n'
+    '1- Calcular novamente \n'
+    '2- Início \n'
+    '3- Ajuda \n'
+    '4- Sair');
+
+    condition = stdin.readLineSync();
+    // WHEN ETAPA > 10 == FINALIZAÇÕES
+    if(etapa == 10 && condition == "1"){
+      calculoMedia();
+    } else if (etapa == 11 && condition == "1"){
+      mediaEmpresarial();
+    }
+
+    if (condition == "2"){
+      main(); 
+    } else if (condition == "3"){
+      help();
+    } else {}
+
+
+  } else if (fim == true){
+    print ('Para onde deseja ir? \n\n'
+    '1- Início \n'
+    '2- Sair');
+
+    if (condition == "1"){
+      main(); 
+    } else if (condition == "2"){
+      help();
+    }
+  }
+}
 
 void help(){
-  clear();
+  
+  commandLine("clear");
+
   print('Preisando de uma ajuda? Seguem a baixo os comandos do SCM! \n \n'
 
   'OBS: SEMPRE INTERPRETE AS OPÇÕES REPONDENDO PELO NÚMERO RESPECTIVO A OPÇÃO!! \n \n'
@@ -129,59 +261,13 @@ void help(){
 }
 
 
+void commandLine(func){
 
-void analiseDeEtapa(condition, etapa, fim){
-  // print('entrou em analise de etapa com $condition');
+  if(func == "clear"){
+    // COMMAND LINE TO CLEAR CMD
+    print("\x1B[2J\x1B[0;0H");
+  } else if (func == "exit"){
 
-  if(condition == help){
-    help();
-  }
-
-  if (etapa == 0){
-    if(condition == "1"){
-      calculoMedia();
-    } else if (condition == "3"){
-      clear();
-      print('Deseja calcular em que modelo? \n'
-      '1 - Bimestral \n'
-      '2 - Trimestral \n'
-      '3 - Anual'); 
-    } else {
-      clear();
-      print('Erro: Escolha uma opção!! \n'
-      '*** Digite "help" para obter ajuda ***');
-      main();
-    }
-  }
-
-  if(fim == true){
-    print ('Para onde deseja ir?');
-    print('');
-    print('1- Calcular novamente');
-    print('2- Início');
-    print('3- Ajuda');
-    print('4- Sair');
-
-    condition = stdin.readLineSync();
-    // WHEN ETAPA > 10 == FINALIZAÇÕES
-    if(etapa == 10 && condition == "1"){
-      clear();
-      calculoMedia();
-    } else if (etapa == 11 && condition == "1"){
-      clear();
-      mediaEmpresarial();
-    }
-
-    if (condition == "2"){
-      clear();
-      main(); 
-    } else if (condition == "3"){
-      help();
-    } else {}
   }
 }
 
-void clear(){
-  // COMMAND LINE TO CLEAR CMD
-  print("\x1B[2J\x1B[0;0H");
-}
